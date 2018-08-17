99a89ebb1e585bd1c36e5069fb0577ae410ee72d
Justin Massey <jmassey_1991@hotmail.com>
2018-08-17 10:58:51 -0400

    Finished SQL project 0 with all stretch goals, junit testing and logging

--- bank-o-tron3000/src/main/java/com/revature/daos/AccountDatabase.java
+++ bank-o-tron3000/src/main/java/com/revature/daos/AccountDatabase.java
@@ -5,13 +5,14 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.apache.log4j.Logger;
+
 import com.revature.beans.Account;
-import com.revature.beans.User;
 import com.revature.utilities.DAOUtilities;
 
 public class AccountDatabase implements AccountDao {
-	
-	Connection connection = null;	// Our connection to the database
+	private Logger log = Logger.getRootLogger();
+	Connection connection = null; // Our connection to the database
 	PreparedStatement stmt = null;
 
 	@Override
@@ -30,26 +31,28 @@ public class AccountDatabase implements AccountDao {
 				stmt.setString(1, a.getAccountNumber());
 				stmt.setInt(2, a.getBalance());
 				stmt.setString(3, a.getAccountType());
-				
+
 				// stmt.executeUpdate();
 				if (stmt.executeUpdate() == 0) {
 					System.out.println("Could not updata database");
+					log.warn("Account doesn't exist but can't add new account to database");
 					return -1;
 				}
-					
+
 				else {
 					System.out.println("Account created successfully");
+					log.info("Account stored in database successfully");
 					return 1;
 				}
-					
+
 			} catch (SQLException e) {
 				e.printStackTrace();
 			} finally {
 				closeResources();
 			}
-		}
-		else {
+		} else {
 			System.out.println("Account already exists");
+			log.info("tried to create account that already exists");
 			return -1;
 		}
 		return 0;
@@ -58,15 +61,18 @@ public class AccountDatabase implements AccountDao {
 
 	@Override
 	public Account findByAccountNumber(String accountNumber) {
-		if (accountNumber == null)
+		if (accountNumber == null) {
+			log.info("There was no account number to find the account with when called");
 			return null;
+		}
 		Account a = new Account();
 		try {
 			connection = DAOUtilities.getConnection();
+			log.info("got new connection for find account");
 			String sql = "SELECT * FROM accounts WHERE accountnumber = ? ;";
 			stmt = connection.prepareStatement(sql);
 			stmt.setString(1, accountNumber);
-			
+
 			ResultSet rs = stmt.executeQuery();
 
 			if (rs.next()) {
@@ -77,12 +83,12 @@ public class AccountDatabase implements AccountDao {
 				stmt = connection.prepareStatement(sql2);
 				stmt.setString(1, a.getAccountNumber());
 				ResultSet rs2 = stmt.executeQuery();
-				while(rs2.next()) {
+				while (rs2.next()) {
 					a.addTransHistory(rs2.getString("transactionhistory"));
 				}
 				return a;
 			} else {
-				// System.out.println("User not found!");
+				log.info("User not found in the database");
 				return null;
 			}
 		} catch (SQLException e) {
@@ -97,6 +103,7 @@ public class AccountDatabase implements AccountDao {
 	public void updateAccount(Account a) {
 		if (a == null) {
 			System.out.println("No account selected");
+			log.info("Tried to update an account that doesn't exist");
 			return;
 		}
 		try {
@@ -108,29 +115,27 @@ public class AccountDatabase implements AccountDao {
 			stmt.setInt(2, a.getBalance());
 			stmt.setString(3, a.getAccountType());
 			stmt.setString(4, a.getAccountNumber());
-			
-			
+
 			// stmt.executeUpdate();
 			if (stmt.executeUpdate() == 0)
 				System.out.println("Could not update database");
-			
-			for(String history: a.getTransHistory()) {
-			String sql2 = "SELECT * FROM account_transaction_history WHERE accountnumber = ? AND transactionhistory = ?;";
-			stmt = connection.prepareStatement(sql2);
-			stmt.setString(1, a.getAccountNumber());
-			stmt.setString(2, history);
-			ResultSet rs = stmt.executeQuery();
-			if (rs.next()) {
-				continue;
-			}
-			else { //  not in the users_account table
-				String sql3 = "INSERT INTO account_transaction_history VALUES (?, ?);";
-				stmt = connection.prepareStatement(sql3);
+
+			for (String history : a.getTransHistory()) {
+				String sql2 = "SELECT * FROM account_transaction_history WHERE accountnumber = ? AND transactionhistory = ?;";
+				stmt = connection.prepareStatement(sql2);
 				stmt.setString(1, a.getAccountNumber());
 				stmt.setString(2, history);
-				if(stmt.executeUpdate() != 0) 
-					System.out.println("Successfully added transaction history to account " + a.getAccountNumber());
-			}
+				ResultSet rs = stmt.executeQuery();
+				if (rs.next()) {
+					continue;
+				} else { // not in the users_account table
+					String sql3 = "INSERT INTO account_transaction_history VALUES (?, ?);";
+					stmt = connection.prepareStatement(sql3);
+					stmt.setString(1, a.getAccountNumber());
+					stmt.setString(2, history);
+					if (stmt.executeUpdate() != 0)
+						System.out.println("Successfully added transaction history to account " + a.getAccountNumber());
+				}
 			}
 //			else
 //				System.out.println("User updated successfully");
@@ -140,15 +145,13 @@ public class AccountDatabase implements AccountDao {
 			closeResources();
 		}
 
-
 	}
 
 	@Override
 	public void deleteAccount(Account a) {
-		
-
+		log.info("Tried to delete an account without it implemented");
 	}
-	
+
 	private void closeResources() {
 		try {
 			if (stmt != null)
--- bank-o-tron3000/src/main/java/com/revature/utilities/DAOUtilities.java
+++ bank-o-tron3000/src/main/java/com/revature/utilities/DAOUtilities.java
@@ -4,18 +4,21 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.apache.log4j.Logger;
+
 public class DAOUtilities {
 	private static final String CONNECTION_USERNAME = "postgres";
 	private static final String CONNECTION_PASSWORD = "password";
 	private static final String URL = "jdbc:postgresql://revature-1808.cxks8zic54ol.us-east-1.rds.amazonaws.com:5432/postgres?currentSchema=bank_o_tron";
 	private static Connection connection;
+	private static Logger log = Logger.getRootLogger();
 	
 	public static synchronized Connection getConnection() throws SQLException {
 		if (connection == null) {
 			try {
 				Class.forName("org.postgresql.Driver");
 			} catch (ClassNotFoundException e) {
-				System.out.println("Could not register driver!");
+				log.info("Could not register driver!");
 				e.printStackTrace();
 			}
 			connection = DriverManager.getConnection(URL, CONNECTION_USERNAME, CONNECTION_PASSWORD);
@@ -23,7 +26,7 @@ public class DAOUtilities {
 		
 		//If connection was closed then retrieve a new connection
 		if (connection.isClosed()){
-			//System.out.println("Opening new connection...");
+			log.info("Opening new connection...");
 			connection = DriverManager.getConnection(URL, CONNECTION_USERNAME, CONNECTION_PASSWORD);
 		}
 		return connection;
